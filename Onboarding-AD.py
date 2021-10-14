86bf0a40045a0e0bfd6fab0d129d174d88e070c5
Brown <Travis.Brown@fdacs.gov>
2021-10-14 09:09:46 -0400

    I hate PowerShell.

---
+++ Onboarding-AD.py
@@ -0,0 +1,19 @@
+﻿#
+# Ask for user first & last name
+    #TODO: Name input validation
+# Ask for user's division
+    #TODO: Present division options
+        #TODO: Division input validation
+
+
+
+
+Import-Module ..\FDACS_data.ps1
+Write-Host $divisions
+# $first_name = Read-Host -Prompt "Enter the user's first name"
+# $last_name = Read-Host "Enter the user's last name"
+# Write-Host "You wrote $first_name $last_name."
+# $confirmation = Read-Host "Is that correct? (Y/N)"
+# foreach ($item in $FDACS_data.divisions) {
+#     Write-Host $item
+# } 
